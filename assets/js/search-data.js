280fa3c6186e8ed70d40c3a380001eaaa919d15e
Alex-Xenos <145714830+Alex-Xenos@users.noreply.github.com>
2026-02-26 20:39:32 +0000

    Deploying to gh-pages from @ Alex-Xenos/Alex-Xenos.github.io@64a4e1ec5b48576ef557164d01fdd67ea0b823d4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-contact",
-          title: "contact",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/contact/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
